ed7605949ff8b1c652a80071ccce5921afe4a092
SahilKumarGit <email.sahil@yahoo.com>
2022-05-04 19:44:45 +0530

    edit book create

--- src/controller/auth/createAccount.js
+++ src/controller/auth/createAccount.js
@@ -2,7 +2,7 @@ const userModule = require('../../module/user.schema')
 
 const createAccount = async (req, res) => {
 
-    // Data comes from body
+    //👇 Data comes from body
     const data = req.body;
     if (Object.keys(data).length == 0) return res.status(400).send({
         status: !true,
@@ -20,9 +20,19 @@ const createAccount = async (req, res) => {
         message: "Phone number must be required"
     })
 
+    if (!isValidMobile(data.phone)) return res.status(400).send({
+        status: !true,
+        message: "Invalid phone number, Try with a valid phone number"
+    })
+
     if (!data.emailId || data.emailId.trim() == '') return res.status(400).send({
         status: !true,
         message: "Email address must be required"
+    })
+
+    if (!isValidEmail(data.emailId)) return res.status(400).send({
+        status: !true,
+        message: "Invalid email address, Try with a valid email address"
     })
 
     if (!data.password || data.password.trim() == '') return res.status(400).send({
@@ -30,6 +40,15 @@ const createAccount = async (req, res) => {
         message: "Password must be required"
     })
 
+    // 👇 email address already exist
+    let emailIsExist = userModule.findOne({
+        emailId: data.emailId
+    }).catch(_ => null)
+
+    if (emailIsExist) return res.status(406).send({
+        status: !true,
+        message: "Email address already exist"
+    })
 
     // 👇 extra  extra  extra  extra  extra
     data.category = [{
@@ -47,7 +66,7 @@ const createAccount = async (req, res) => {
 
 
     try {
-        // create user in users DB
+        //👇 create user in users DB
         const createUser = await userModule.create(data);
         res.status(200).send({
             status: true,
@@ -60,6 +79,23 @@ const createAccount = async (req, res) => {
         })
     }
 }
+
+
+
+
+
+//👇 validate email address 📧
+const isValidEmail = (email) => {
+    let regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return regEx.test(email)
+}
+
+//👇 validate phone number ☎️
+const isValidMobile = (number) => {
+    let regEx = /^[6-9]\d{9}$/
+    return regEx.test(number)
+}
+
 
 
 module.exports = createAccount
