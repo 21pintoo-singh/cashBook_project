4e26d0b1b084930d5cb2fd00a4f9d493fa20e131
SahilKumarGit <email.sahil@yahoo.com>
2022-05-04 20:00:02 +0530

    basic changes

--- src/module/cash.schema.js
+++ src/module/cash.schema.js
@@ -4,41 +4,42 @@ const Objectid = mongoose.Schema.Types.ObjectId
 const cashschema = new mongoose.Schema({
     amount: {
         type: Number,
-        trim:true,
+        trim: true,
         required: "enter amount"
     },
     category: {
         type: [String],
-        trim:true,
+        trim: true,
         required: "payment category"
     },
     date: {
         type: Number,
-        trim:true,
+        trim: true,
         default: new Date().getTime()
     },
     title: {
         type: String,
-        trim:true,
+        trim: true,
         required: "title is mandatory"
     },
     userId: {
         type: Objectid,
-        trim:true,
+        trim: true,
         ref: 'user',
         required: "User ID is mandatory"
     },
     bookId: {
         type: Objectid,
-        trim:true,
+        trim: true,
         ref: 'book',
         required: "Book ID is mandatory"
     },
-    isDeleted:{
-        type:Boolean,
-        trim:true,
-        default:false
+    isDeleted: {
+        type: Boolean,
+        trim: true,
+        default: false
     }
-},
-    { timestamps: true })
+}, {
+    timestamps: true
+})
 module.exports = mongoose.model('payment', cashschema)
