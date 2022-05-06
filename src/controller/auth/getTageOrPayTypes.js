751a2c47bd8c1d03680f0c6309277fa78554e287
SahilKumarGit <email.sahil@yahoo.com>
2022-05-06 15:19:01 +0530

    listing categories and payment type

---
+++ src/controller/auth/getTageOrPayTypes.js
@@ -0,0 +1,64 @@
+const userModule = require('../../module/user.schema')
+
+const categoryList = async (req, res) => {
+    try {
+        const userId = req.decodeToken.user;
+
+        const userData = await userModule.findOne({
+            _id: userId,
+            isDelete: false
+        }).catch(_ => null)
+
+        //👇 check 'categoryListing' is exist OR 'category' exist OR the 'length'
+        if (!userData || !userData.category || userData.category.length == 0) return res.status(404).send({
+            status: false,
+            data: 'No category(s) list found'
+        })
+
+        res.status(200).send({
+            status: true,
+            data: userData.category
+        })
+
+    } catch (e) {
+        res.status(500).send({
+            status: false,
+            data: e.message
+        })
+    }
+}
+
+
+const paymentTypeList = async (req, res) => {
+    try {
+        const userId = req.decodeToken.user;
+
+        const userData = await userModule.findOne({
+            _id: userId,
+            isDelete: false
+        }).catch(_ => null)
+
+        //👇 check 'paymentMethodsListing' is exist OR 'paymentMethods' exist OR the 'length'
+        if (!userData || !userData.paymentMethods || userData.paymentMethods.length == 0) return res.status(404).send({
+            status: false,
+            data: 'No paymentMethods(s) list found'
+        })
+
+        res.status(200).send({
+            status: true,
+            data: userData.paymentMethods
+        })
+
+    } catch (e) {
+        res.status(500).send({
+            status: false,
+            data: e.message
+        })
+    }
+}
+
+
+module.exports = {
+    categoryList,
+    paymentTypeList
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,29 +2,31 @@ const express = require('express');
 const authC = require('../middleWare/authentication')
 const authZ = require('../middleWare/authorization')
 const createAccount = require('../controller/auth/createAccount')
-const log = require('../controller/auth/login')
+const login = require('../controller/auth/login')
 const createBook = require('../controller/books/create')
 const getBookList = require('../controller/books/getList')
 const updateBook = require('../controller/books/update')
 const deleteBook = require('../controller/books/delete')
-const payment=require('../controller/payment/create')
-const delpay=require('../controller/payment/deletepayment')
-const listpay=require('../controller/payment/getPaymentfromBook')
-
+const payment = require('../controller/payment/create')
+const delpay = require('../controller/payment/deletepayment')
+const listpay = require('../controller/payment/getPaymentfromBook')
+const allTags = require('../controller/auth/getTageOrPayTypes')
 
 const router = express.Router();
 
 
 router.post('/register', createAccount)
- router.post('/login',log)
+router.post('/login', login)
+router.get('/user/categoryList', authC, allTags.categoryList)
+router.get('/user/paymentTypeList', authC, allTags.paymentTypeList)
 
 router.post('/createBook', authC, createBook)
 router.get('/listBook', authC, getBookList)
 router.post('/updateBook/:bookId', authC, authZ.byBook, updateBook)
 router.delete('/deleteBook/:bookId', authC, authZ.byBook, deleteBook)
-router.post('/CreatePayment',payment.createpayment)
-router.delete('/deletePayment/:paymentId',authC,authZ.byPayment,delpay.del)
-router.get('/listofPayment/:bookId',authC,authZ.byBook,listpay)
+router.post('/CreatePayment', payment.createpayment)
+router.delete('/deletePayment/:paymentId', authC, authZ.byPayment, delpay.del)
+router.get('/listofPayment/:bookId', authC, authZ.byBook, listpay)
 
 
 
