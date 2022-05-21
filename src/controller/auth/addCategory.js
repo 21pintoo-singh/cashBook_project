72b79057ea1ac9bf2acc89cd3ee4f6741582adbe
SahilKumarGit <email.sahil@yahoo.com>
2022-05-21 20:05:28 +0530

    add category and pay!

---
+++ src/controller/auth/addCategory.js
@@ -0,0 +1,39 @@
+const usermodel = require('../../module/user.schema')
+
+const addCategory = async (req, res) => {
+    try {
+        const category = req.body.category;
+        if (!category) return res.status(400).send({
+            status: false,
+            message: "Category must be required!"
+        })
+
+        const userId = req.decodeToken.user;
+        const getUser = await usermodel.findOne({
+            _id: userId,
+            isDeleted: false
+        })
+        if (!getUser) return res.status(404).send({
+            status: false,
+            message: "No such user found"
+        })
+        if (getUser.category.indexOf(category) !== -1) return res.status(400).send({
+            status: false,
+            message: "Category already exist!"
+        })
+        //let arr=[]
+        getUser.category.push(category)
+        await getUser.save();
+        res.status(200).send({
+            status: true,
+            Data: getUser.category
+        })
+    } catch (_) {
+        res.status(500).send({
+            status: false,
+            message: _.message
+        })
+    }
+}
+
+module.exports = addCategory
---
+++ src/controller/auth/addPaymentMethods.js
@@ -0,0 +1,39 @@
+const usermodel = require('../../module/user.schema')
+
+const addPaymentMethods = async (req, res) => {
+    try {
+        const paymentMethods = req.body.paymentMethods;
+        if (!paymentMethods) return res.status(400).send({
+            status: false,
+            message: "paymentMethods must be required!"
+        })
+
+        const userId = req.decodeToken.user;
+        const getUser = await usermodel.findOne({
+            _id: userId,
+            isDeleted: false
+        })
+        if (!getUser) return res.status(404).send({
+            status: false,
+            message: "No such user found"
+        })
+        if (getUser.paymentMethods.indexOf(paymentMethods) !== -1) return res.status(400).send({
+            status: false,
+            message: "paymentMethods already exist!"
+        })
+        //let arr=[]
+        getUser.paymentMethods.push(paymentMethods)
+        await getUser.save();
+        res.status(200).send({
+            status: true,
+            Data: getUser.paymentMethods
+        })
+    } catch (_) {
+        res.status(500).send({
+            status: false,
+            message: _.message
+        })
+    }
+}
+
+module.exports = addPaymentMethods
